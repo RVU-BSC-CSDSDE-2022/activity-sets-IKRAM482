be9480c497808ef93abe2414b5887d49a9f2bc59
Mohammed  Ikram <115693724+IKRAM482@users.noreply.github.com>
2023-01-08 10:21:40 +0000

    solved

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 int input();
 int add(int a, int b);
--- set02/problem03.c
+++ set02/problem03.c
@@ -4,5 +4,43 @@ int is_composite(int n);
 void output(int n, int result);
 int input_number()
 {
+  int n;
+  printf("enter a number:");
+  scanf("%d",&n);
+  return n;
+}
+int is_composite(int n)
+{
+  int i,p,result;
+    
+  for(i=2; i<=n/2;i++)
+    {
+      if((i%n)==0)
+        {
+        p=1;
+        break;
+        }
+    }
+  if(p==0)
+  {
+    printf("It is prime number\n");
+  }
+  else
+  {
+    printf("It is composite number\n");
+  }
+  return result;
   
+}
+void output(int n, int result)
+{
+  printf("the composite of %d is %d",n,result);
+}
+int main()
+{
+  int n, result;
+  n=input_number();
+  result=is_composite(n);
+  output(n,result);
+  return(0);
 }
